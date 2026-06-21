007522015f8977f0427c83f5a6afae2902fc2c50
Esther Liu <ezekielgarden@gmail.com>
2026-06-20 20:13:43 -0700

    Cache versioned PWA assets for offline use

--- sw.js
+++ sw.js
@@ -2,27 +2,27 @@ const CACHE = "pawhelp-writer-v5";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css",
-  "./js/app-01.js",
-  "./js/app-02.js",
-  "./js/app-03.js",
-  "./js/app-04.js",
-  "./js/app-05.js",
-  "./js/app-06.js",
-  "./js/app-07.js",
-  "./js/app-08.js",
-  "./js/app-09.js",
-  "./js/app-10.js",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/apple-touch-icon.png"
-  ,"./assets/photos/voice-cat.jpg"
-  ,"./assets/photos/paper-pets.jpg"
-  ,"./assets/photos/urgent-cat.jpg"
-  ,"./assets/photos/starlight-cat.jpg"
-  ,"./assets/photos/polaroid-cat.jpg"
-  ,"./assets/photos/editorial-dog.jpg"
+  "./styles.css?v=5",
+  "./js/app-01.js?v=5",
+  "./js/app-02.js?v=5",
+  "./js/app-03.js?v=5",
+  "./js/app-04.js?v=5",
+  "./js/app-05.js?v=5",
+  "./js/app-06.js?v=5",
+  "./js/app-07.js?v=5",
+  "./js/app-08.js?v=5",
+  "./js/app-09.js?v=5",
+  "./js/app-10.js?v=5",
+  "./manifest.webmanifest?v=5",
+  "./icons/icon-192.png?v=5",
+  "./icons/icon-512.png?v=5",
+  "./icons/apple-touch-icon.png?v=5",
+  "./assets/photos/voice-cat.jpg?v=5",
+  "./assets/photos/paper-pets.jpg?v=5",
+  "./assets/photos/urgent-cat.jpg?v=5",
+  "./assets/photos/starlight-cat.jpg?v=5",
+  "./assets/photos/polaroid-cat.jpg?v=5",
+  "./assets/photos/editorial-dog.jpg?v=5"
 ];
 
 self.addEventListener("install", (event) => {
